813cae193e61651aabddcf003c4a789b3fd2f2e9
Harshal-SL <code.aimbot@gmail.com>
2025-07-31 12:26:03 +0000

    Modification in Code

--- Day1/Day1_Part1.c
+++ Day1/Day1_Part1.c
@@ -1,50 +1,107 @@
+//Implementation without Default libraries
+
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 1000
+// #define MAX 1000
 
-//Custom Comparator using for QSort funtion to Sort elements in ascending Order
-int compare(const void*a,const void*b){
-    return(*(int*)a - *(int*)b);
+//Implementation of the Quick Sort Function 
+
+// Swap the Elements
+void swap(long int *a,long int *b){
+    long int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-// Calcuate Total Distance
-int totaldistance(int* left,int* right,int size){
 
-    // Sort the array elements
-    qsort(left,size,sizeof(int),compare);
-    qsort(right,size,sizeof(int),compare);
+int partition(long int arr[], long int low, long int high){
+    long int pivot = arr[low];
+    long int i = low + 1;
+    long int j = high;
 
-    int total = 0;
-    
+    while(i <= j){
+        while(i <= high && arr[i] <= pivot) i++;
+        while(j >= low && arr[j] > pivot) j--;
+
+        if(i < j){
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[low], &arr[j]); 
+    return j;
+}
+
+void quick_sort(long int arr[], long int low, long int high){
+    if(low < high){
+        int p = partition(arr, low, high);
+        quick_sort(arr, low, p - 1);
+        quick_sort(arr, p + 1, high);
+    }
+
+} //problem
+
+
+long int Abs(long int x){
+    // if(x < 0){
+    //     return x*-1;
+    // }
+    // return x;
+    return (x<0)? -x:x;
+}
+
+long int totaldistance(long int left[],long int right[],long int size){
+    quick_sort(left,0,size-1);
+    quick_sort(right,0,size-1);
+    long int total = 0;
+
     // Calcuate the Total Distance
-    for(int i = 0 ; i < size ; i++){
-        total += abs(left[i] - right[i]);
+    for(long int i = 0 ; i < size ; i++){
+        total += Abs(left[i] - right[i]);
     }
     return total;
 }
 
-
-
-
 int main(){
-
     //Read the Input File
-    FILE *fp = fopen("input.txt","r");
+    FILE *fp = fopen("input2.txt","r");
 
     if(fp == NULL){
         printf("Error in opening the file");
-        return 1;
+        return 1; 
     }
 
-    int left[MAX] , right[MAX];
-    int count = 0; 
+    // Implement using Dynamic Array using Malloc and Realloc
 
-    //Read the elements of the input file 
-    while(fscanf(fp,"%d %d",&left[count],&right[count]) == 2){
-        count++;
+    long int max = 1000; 
+    long int *left = malloc(max*sizeof(long int)); // Initize the Left array
+    long int *right = malloc(max*sizeof(long int)); // Initize the Right array
+    long int count = 0; 
+    char *line = NULL;
+    size_t line_len = 0;
+
+    while(getline(&line,&line_len, fp) != -1){ // Traverse Line by line 
+        if(sscanf(line,"%ld %ld",&left[count],&right[count])==2){ // Check the contain of the line will match the format required
+            if(count >= max){// Check whether if the number for element is more then the initalize value
+                //Relocate the size of the array by 2 
+                max *= 2;
+                long int *temp_left = realloc(left, max*sizeof(long int));
+                long int *temp_right = realloc(right, max*sizeof(long int));  
+                //Assign the relocate size to the arrays            
+                left = temp_left;
+                right = temp_right;
+            }   
+            count++;    
+        }
     }
 
-    int result = totaldistance(left, right, count); // Call Total Distance Function
-    printf("Total Distance is = %d\n",result);
+
+    fclose(fp);
+    long int result = totaldistance(left, right, count); // Call Total Distance Function
+    printf("Total Distance is = %ld\n",result);
+
+    //Delocate the memory
+    free(left);
+    free(right);
     return 0;
 }
---
+++ Day2/Part1_with_modifications.c
@@ -0,0 +1,88 @@
+// Using Dynamic Memory allocation
+
+#include<stdio.h>
+#include <stdlib.h>
+
+// Detemine whether the level is safe.
+int is_safe(int arr[] , int n){
+
+    // Check Whether the adjecent elements are equal
+    for(int i = 1 ; i < n ; i++){
+        if(arr[i] == arr[i-1]){   
+            return 0;
+        }
+    }
+
+    // Detemine Whether the level is increasing or decreasing
+    int increasing = arr[1] > arr[0];
+    int decreasing = arr[0] > arr[1];
+
+
+    for (int i = 1; i < n; i++) {
+        int difference = arr[i] - arr[i-1]; // Difference between the corresponding elements
+        
+        // Check if increasing the difference should be greater then 0 , similarily is decreasing the difference must be lesser then 0
+        if((increasing && difference <=0 ) || (decreasing && difference >= 0))
+            return 0;
+        
+        // Check the difference between is exactly in range between 1 - 3 else return false
+        if (difference > 3 || difference < -3)
+            return 0;
+    }
+
+    return 1;
+}
+
+
+int main(){
+
+    int total_safe = 0;
+    FILE *fp;
+
+    fp = fopen("input.txt","r"); // Open the input file
+    char *line = NULL;
+    size_t line_len = 0;
+
+    // Read the input file line by line
+    while(getline(&line,&line_len,fp) != -1) {
+        int max = 5;
+        int *level = malloc(max*sizeof(int));    // Create an array to store the element of the perticular level
+        int n = 0;
+
+        char *ptr = line; //Create a pointer to traverse the elements within the line 
+
+        // Read the element of the level and store in the array 
+        while(sscanf(ptr,"%d",&level[n]) == 1){
+
+            if( n >= max){
+                max *= 2;
+                level = realloc(level,max*sizeof(int));
+                
+            }
+            n++;
+            while(*ptr != ' ' && *ptr != '\0') ptr++; // Skips the current number 
+            while(*ptr == ' ' ) ptr++ ;// Skips any empty spaces in between the elements
+        } 
+        // Call the is_safe function for that perticular level
+        if (n > 0 && is_safe(level, n)) {
+            total_safe++; // If the level is safe then increment the total_safe
+        }
+        free(level);
+  
+    }
+    // int arr1[] = {7, 6, 4, 2, 1};
+    // int arr2[] = {1, 2, 7, 8, 9};
+    // int arr3[] = {9, 7, 6, 2, 1};
+    // int arr4[] = {1, 3, 2, 4, 5};
+    // int arr5[] = {8, 6, 4, 4, 1};
+    // int arr6[] = {1, 3, 6, 7, 9};
+
+    // if (is_safe(arr6, 5)) {
+    //       total_safe++;
+    //     }
+    
+    printf("Total Safe : %d \n" ,total_safe);
+    fclose(fp); // Close the opened file
+    
+    return 0;
+}
